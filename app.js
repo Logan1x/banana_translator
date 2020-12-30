8bff89e35601d3f85c5ef77beb6d1ca19e1dac38
noob <sharmakhushal78@gmail.com>
2020-12-30 11:33:24 +0530

    testing api done now on minion api

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var translate = document.querySelector("#btn-translate");
+var input_translate = document.querySelector("#input-translate")
+var output_translate = document.querySelector("#output-translate")
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?" + "text=" + input_translate.value
 
-function callback(){
-    console.log("Clicked")
+function callback() {
+    fetch(url)
+        .then(response => response.json())
+        .then(json => { 
+            var output_text = json.contents.translated;
+            output_translate.innerText = output_text;
+
+        })
+    output_translate.innerText = input_translate.value;
 }
 
 translate.addEventListener("click", callback)
