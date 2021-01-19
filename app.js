dcc89d0f73d310b095a5b8d38cb0e8912203417a
noob <sharmakhushal78@gmail.com>
2021-01-19 19:12:54 +0530

    now with dark theme cache

--- app.js
+++ app.js
@@ -30,16 +30,37 @@ function callback() {
 translate.addEventListener("click", callback)
 
 
-var currentTheme = 'dark';
+// dark theme
+
+// var currentTheme = 'dark';
+
+// function changeTheme() {
+//     document.body.classList.toggle('dark-mode');
+
+//     if (currentTheme === 'dark') {
+//         document.getElementById('toggleknop').innerHTML = '<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+//         currentTheme = 'sun';
+//     } else {
+//         document.getElementById('toggleknop').innerHTML = '<i class="fas fa-moon" id="maan" style="color:#737eac;"></i>';
+//         currentTheme = 'dark';
+//     }
+// }
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+    document.getElementById('toggleknop').innerHTML = '<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+  document.body.classList.add("dark-theme");
+}
 
 function changeTheme() {
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle("dark-theme");
+  
+  document.getElementById('toggleknop').innerHTML = '<i class="fas fa-moon" id="maan" style="color:#737eac;"></i>';
 
-    if (currentTheme === 'dark') {
-        document.getElementById('toggleknop').innerHTML = '<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
-        currentTheme = 'sun';
-    } else {
-        document.getElementById('toggleknop').innerHTML = '<i class="fas fa-moon" id="maan" style="color:#737eac;"></i>';
-        currentTheme = 'dark';
-    }
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    document.getElementById('toggleknop').innerHTML = '<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
 }
